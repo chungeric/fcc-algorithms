360795244a5136ecffd5380c6dedde9aebd0c534
Eric Chung <rchung.eric@gmail.com>
2018-01-27 19:17:40 +1100

    lost access to first attempts, added chunky monkey solution

--- fcc-basic-algorithm-scripting/10 Chunky Monkey/es6/10-chunky-monkey.js
+++ fcc-basic-algorithm-scripting/10 Chunky Monkey/es6/10-chunky-monkey.js
@@ -1,13 +1,8 @@
 function chunkArrayInGroups(arr, size) {
-  let group = [];
-  return arr.reduce((acc,cur,i) => {
-    group.push(cur);
-    if ((i+1) % size === 0) {
-      acc.push(group);
-      group = [];
-    }
-    return acc;
-  },[]);
+  for (i = 0, outer = []; i < arr.length; i+=size) {
+    outer.push(arr.slice(i, i+size));
+  }
+  return outer;
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
